30334ce26b2670480ba4c936aaa2ec274ddeafb2
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-08-16 13:12:29 +0600

    Login and register routes added

--- src/Router/index.ts
+++ src/Router/index.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import { CourseRoutes } from "../Modules/Courses/Courses.routes";
 import { moduleRoutes } from "../Modules/CourseModules/Modules.routes";
 import { lectureRoutes } from "../Modules/Lectures/Lecture.routes";
+import { RegistrationRoutes } from "../Modules/Auth/Registration/auth.routes";
+import { LoginRoutes } from "../Modules/Auth/Login/login.routes";
 
 const router = Router();
 router.use("/course", CourseRoutes);
 router.use("/module", moduleRoutes);
 router.use("/lecture", lectureRoutes);
+router.use("/auth/register", RegistrationRoutes);
+router.use("/auth/login", LoginRoutes);
 
 export default router;
