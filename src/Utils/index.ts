c6ffe021f79f6fc87bf1bf43f39d2e7280182ac1
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-08-16 13:27:55 +0600

    Auth middle weare added

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,3 +1,16 @@
+import { userRole } from "../Constant";
+import { authMiddleware } from "../MiddleWears/authMiddleWare";
+
+const bothUsers = authMiddleware(userRole.user, userRole.admin);
+const admin = authMiddleware(userRole.admin);
+const user = authMiddleware(userRole.user);
+
+export const permission = {
+  bothUsers,
+  admin,
+  user,
+};
+
 export const isPasswordChange = (utcPass: Date, jwtPass: number) => {
   const passwordChangedAt = Math.floor(new Date(utcPass).getTime() / 1000);
   return passwordChangedAt > jwtPass;
